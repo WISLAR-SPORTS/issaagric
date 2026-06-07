c5916651e9f75bcd281334ffd68f565da0297514
WISLAR SPORTS <wamaniissa0@gmail.com>
2026-06-07 11:03:05 +0300

    Prevent settings query before table exists

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-
 echo "Running migrations..."
 php artisan migrate --force
 
-echo "Starting PHP built-in server..."
+echo "Clearing cache..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+echo "Starting server..."
 php -S 0.0.0.0:8080 -t public
